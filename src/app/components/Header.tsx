623e38b7465ee0d37e5591802e233247ee514937
rhanziy <thdfksdl0029@gmail.com>
2024-07-15 20:16:21 +0900

    design: nav margin add

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,11 +4,9 @@ import styles from "./header.module.css";
 import CustomLink from "./CustomLink";
 import useHeaderVisible from "@hooks/useHeaderVisible";
 import Image from "next/image";
-import useResponsive from "@hooks/useResponsive";
 import { Box } from "@mui/material";
 
 function Header() {
-  const { isMobile } = useResponsive();
   const { isHeaderVisible, isTop } = useHeaderVisible();
 
   return (
@@ -19,13 +17,13 @@ function Header() {
         }`}
       >
         <Box className={styles.container}>
-          <Link href="/" scroll={false}>
-            <Box
-              sx={{
-                width: 240,
-                height: 30,
-              }}
-            >
+          <Box
+            sx={{
+              width: 240,
+              height: 30,
+            }}
+          >
+            <Link href="/" scroll={false}>
               <Image
                 src="/images/logo-horizon.png"
                 alt="로고 이미지"
@@ -34,14 +32,14 @@ function Header() {
                 layout="responsive"
                 priority
               />
-            </Box>
-          </Link>
-          <nav className={styles.nav}>
+            </Link>
+          </Box>
+          <Box className={styles.nav}>
             <CustomLink href="/">소개</CustomLink>
             {/* <CustomLink href="/board">게시물</CustomLink> */}
             <CustomLink href="/reviews">상담후기</CustomLink>
             <CustomLink href="/contact">상담신청</CustomLink>
-          </nav>
+          </Box>
         </Box>
       </Box>
     </>
