c3130cfaf8b34898cddd66fdea6900997673bd70
rhanziy <thdfksdl0029@gmail.com>
2024-09-05 21:27:15 +0900

    rename: 파일명 변경

--- src/app/home/CopyButton.tsx
+++ src/app/home/CopyButton.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import { Tooltip } from "./ToolTip";
+import { Tooltip } from "./Tooltip";
 import theme from "@/app/styles/theme.css";
 
 export const CopyButton: React.FC = () => {
